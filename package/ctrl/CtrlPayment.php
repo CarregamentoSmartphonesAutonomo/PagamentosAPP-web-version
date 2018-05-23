b440ddb39a85c2f3b180db3f89c82da8cbdb1682
Lucas Rufino <lucaslrt@outlook.com>
2018-05-22 22:46:23 -0300

    Removing debug .txt creation

--- package/ctrl/CtrlPayment.php
+++ package/ctrl/CtrlPayment.php
@@ -11,18 +11,6 @@
     $transaction->charge();
     $status = $transaction->status;
 
-
-
-    $file = fopen('token.txt', 'w');
-    fwrite($file, $_POST['product_id']."\n");
-    fwrite($file, ($_POST['value'] * 100)."\n");
-    fwrite($file, $_POST['token']."\n");
-    fwrite($file, $_POST['parcels']."\n");
-    fwrite($file, $status."\n");
-    fclose($file);
-
-
-
     if( strcasecmp($status, 'refused') == 0 ){
         echo '"Pagamento recusado. Tente outro cartão."';
     }
